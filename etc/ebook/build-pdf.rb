66b19fdaff65a0038b969b5e7f253862615f825b
Jay V <air@live.ca>
2025-01-01 21:27:10 -0500

    updating ebook script

--- etc/ebook/build-pdf.rb
+++ etc/ebook/build-pdf.rb
@@ -84,7 +84,7 @@ def build_chapter chapter_data
 
     # Update for Ruby 3.2
     # chapter_front_matter = YAML.load_file("../../_chapters/#{chapter_name}.md")
-    chapter_front_matter = YAML.unsafe_load_file("../../_chapters/#{chapter_name}.md")
+    chapter_front_matter = YAML::load(File.read("../../_chapters/#{chapter_name}.md"))
 
     #################
     # Validations
